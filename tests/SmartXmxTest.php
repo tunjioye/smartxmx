1ba3d9e58102f3a15a5fdb8b4fc8bc939aedafe1
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-03-20 21:20:55 +0100

    update username and password notification

--- tests/SmartXmxTest.php
+++ tests/SmartXmxTest.php
@@ -3,10 +3,10 @@
 
     use SmartXmx\SmartXmx;
 
-    // Smart SMS Solutions LOGIN
+    // Provide Your Smart SMS Solutions LOGIN
+    // update username and password below
     $sms = new SmartXmx("username", "password");
 
     // check Sms Balance
     echo $sms::checkSmsBalance();
-
 ?>
