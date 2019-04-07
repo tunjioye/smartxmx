92a77d7f29e6e33e3439236cf398b564933a1e5a
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2019-04-07 14:51:17 +0100

    Test interpret Response

--- tests/SmartXmxTest.php
+++ tests/SmartXmxTest.php
@@ -7,9 +7,14 @@ use Tunjioye\SmartXmx\SmartXmx;
 
 class SmartXmxTest extends PHPUnit_Framework_TestCase {
     /**
+     * Test SmartXmx::interpretResponse()
+     */
+    public function testInterpretResponse() {
+        $this->assertEquals('SMS Sending Failed', SmartXmx::interpretResponse('2904'));
+    }
+
+    /**
      * Test SmartXmx::checkSmsBalance() returns TRUE if response is numeric
-     *
-     * @return bool
      */
     public function testCheckSmsBalance() {
         $sms = new SmartXmx("username", "password");
